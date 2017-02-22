e1ced8c6dd3e4894bace515e5ba9b7a9185ef63c
jerry <568384493@qq.com>
2017-02-22 17:33:35 +0800

    添加注册页面

--- Demo/blog/config/routes.js
+++ Demo/blog/config/routes.js
@@ -1,18 +1,22 @@
-module.exports=function(app){
+module.exports = function (app) {
     console.log('进入我们路由的配置文件');
-    app.get('/',function(req,res,next){
-      res.render('index',{title:'Express'});
+    app.get('/', function (req, res, next) {
+        res.render('index', { title: 'Express' });
     });
 
-    app.get('/login',function(req,res,next){
-        res.render('login',{title:'登陆'});
+    app.get('/login', function (req, res, next) {
+        res.render('login', { title: '登陆' });
     });
 
-    app.get('/reg',function(req,res,next){
-        res.render('reg',{title:"注册"})
+    app.get('/reg', function (req, res, next) {
+        res.render('reg', { title: "注册" })
     });
 
-    app.get('/logout',function(req,res,next){
-        res.render('logout',{title:"退出"})
+    app.post('/reg', function (req, res, next) {
+        res.render('注册成功');
+    });
+
+    app.get('/logout', function (req, res, next) {
+        res.render('logout', { title: "退出" })
     });
 }
