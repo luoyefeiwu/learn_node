0783fb5fe493a65567afe3b910154fe871ee71ef
jerry <568384493@qq.com>
2017-03-14 17:54:25 +0800

    My blog

--- Demo/myblog/routes/index.js
+++ Demo/myblog/routes/index.js
@@ -1,10 +1,9 @@
-module.exports=function(app){
-  app.get('/',function(req,res){
-      res.redirect('/posts');
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    res.redirect('/posts');
   });
-
-  app.use('/signup',require('./signup'));
-  app.use('/signin',require('./signin'));
-  app.use('/signout',require('./signout'));
-  app.use('/ports',require('./posts'));
-}
+  app.use('/signup', require('./signup'));
+  app.use('/signin', require('./signin'));
+  app.use('/signout', require('./signout'));
+  app.use('/posts', require('./posts'));
+};
