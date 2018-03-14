1662ebce3a10631c1244f6a98fa2581933ab0f32
jerry <56838493@qq.com>
2018-03-15 00:03:37 +0800

    添加 users表

---
+++ myshop/models/users.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose')
+var userSchema = new mongoose.Schema({
+    "userId": String,
+    "userName": String,
+    "userPwd": String,
+    "orderList": Array,
+    "cartList": [{
+        "productId": String,
+        "productName": String,
+        "productImage": String,
+        "checked": String,
+        "productNum": number
+    }
+    ],
+    "addressList": Array
+});
+
+module.exports = mongoose.model("User", userSchema, "users");
