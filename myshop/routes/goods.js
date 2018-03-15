4ff144d79ea4084970eb64e7d29d03dfc25174f7
jerry <568384493@qq.com>
2018-03-15 21:27:55 +0800

    购物车

--- myshop/routes/goods.js
+++ myshop/routes/goods.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Goods = require('../models/goods');
-var config=require('../config/default');
+var config = require('../config/default');
 
 //连接Mongodb数据库
 mongoose.connect(config.mongodb);
@@ -35,29 +35,29 @@ router.get("/list", function (req, res, next) {
             case '2': priceGt = 500; priceLte = 1000; break;
             case '3': priceGt = 1000; priceLte = 5000; break;
         }
-        params={
-            salePrice:{
-                $gt:priceGt,
-                $lt:priceLte
+        params = {
+            salePrice: {
+                $gt: priceGt,
+                $lt: priceLte
             }
         }
     }
-    let goodsModel=Goods.find(params).skip(skip).limit(pageSize);
-    goodsModel.sort({'salePrice':sort});
-    goodsModel.exec(function(err,doc){
-        res.setHeader('Access-Control-Allow-Origin','*');
-        if(err){
+    let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
+    goodsModel.sort({ 'salePrice': sort });
+    goodsModel.exec(function (err, doc) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        if (err) {
             res.json({
-                status:1,
-                msg:err.message
+                status: 1,
+                msg: err.message
             });
-        }else{
+        } else {
             res.json({
-                status:'0',
-                msg:'',
-                result:{
-                    count:doc.length,
-                    list:doc
+                status: '0',
+                msg: '',
+                result: {
+                    count: doc.length,
+                    list: doc
                 }
             });
         }
@@ -65,4 +65,58 @@ router.get("/list", function (req, res, next) {
     });
 
 });
+
+//加入到购物车
+router.post('/addCart', function (req, res, next) {
+   
+    var userId = '1000007', productId = req.body.productId;
+    var User = require('../models/users');
+    User.findOne({ userId: userId }, function (err, result) {
+        if (err) {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.json({
+                status: "1",
+                msg: err.message
+            })
+        } else {
+            console.log('result:' + result);
+            if (result) {
+                Goods.findOne({ productId: productId }).then((err1, doc) => {
+                    if (err1) {
+                        res.setHeader('Access-Control-Allow-Origin', '*');
+                        res.json({
+                            status: "1",
+                            msg: err1.message
+                        });
+                    } else {
+                        if (doc) {
+                            doc.productNum = 1;
+                            doc.checked = 1;
+                            User.cartList.push(doc);
+                            User.save((err2, doc2) => {
+                              
+                                if (err2) {
+                                    res.setHeader('Access-Control-Allow-Origin', '*');
+                                    res.json({
+                                        status: "1",
+                                        msg: err2.message
+                                    });
+                                } else {
+                                    res.setHeader('Access-Control-Allow-Origin', '*');
+                                    res.json({
+                                        status: "0",
+                                        msg: "",
+                                        result: 'suc'
+                                    });
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        }
+    });
+
+});
+
 module.exports = router;
--- myshop/routes/users.js
+++ myshop/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose=require('mongoose');
-var User = require('./../models/user');
+var User = require('./../models/users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
