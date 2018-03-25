171077c58d538c93331f371f3ea6c8501ea38922
jerry <568384493@qq.com>
2018-03-26 00:05:06 +0800

    ‘购物车接口模块’

--- myshop/routes/users.js
+++ myshop/routes/users.js
@@ -86,6 +86,20 @@ router.get('/cartList', function (req, res, netx) {
     }
   });
 });
+//编辑购物车
+router.post("/cartEdit", function (req, res, next) {
+  var userId = req.cookies.userId,
+    productId = req.body.productId,
+    productNum = req.body.productNum,
+    checked = req.body.checked;
+  User.update({ 'userId': userId, 'cartList.productId': productId }, { 'cartList.$.productNum': productNum, 'cartList.$.checked': checked }, function (err, doc) {
+    if (err) {
+      res.json({ status: '1', msg: err.message, result: '' });
+    } else {
+      res.json({ status: '0', msg: '', result: 'suc' });
+    }
+  });
+});
 //删除购物车
 router.post('/delCart', function (req, res, next) {
   let productId = req.body.productId, userId = req.cookies.userId;
@@ -110,6 +124,35 @@ router.post('/delCart', function (req, res, next) {
       });
     }
   });
+});
+
+//全选或者取消全选
+router.post('/editCheckAll', function (req, res, next) {
+  let userId = req.cookies.userId,
+    checkAll = req.body.checkAll?'1':'0';
+
+  User.findOne({ userId: userId }, function (err, user) {
+    if (err) {
+      res.json({ status: '1', msg: err.message, result: '' });
+    } else {
+      if (user) {
+        user.cartList.forEach((item) => {
+          item.checked = checkAll;
+        });
+        user.save(function (err1, doc1) {
+          if (err1) {
+            res.json({ status: '1', msg: err1.message })
+          } else {
+            res.json({ status: '0', msg: '', result: 'suc' });
+          }
+        });
+      } else {
+        res.json({ status: '1', msg: '', result: '' });
+      }
+    }
+  });
+
+
 });
 
 module.exports = router;
