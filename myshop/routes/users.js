9815a34ccc14e4045b7c66ea21faf8c06ff8d7c1
jerry <568384493@qq.com>
2018-04-02 00:23:58 +0800

    Order Interface

--- myshop/routes/users.js
+++ myshop/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('./../models/users');
+require('./../util/util')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -178,16 +179,140 @@ router.post('/setDefault', function (req, res, netx) {
         } else {
           item.isDefault = false;
         }
-        doc.save(function (err1, doc1) {
-          if (err1) {
-            res.json({ status: '1', msg: err.message, result: '' });
-          } else {
-            res.json({ status: '0', msg: '', result: '' });
-          }
-        });
+      });
+      doc.save(function (err1, doc1) {
+        if (err1) {
+          res.json({ status: '1', msg: err.message, result: '' });
+        } else {
+          res.json({ status: '0', msg: '', result: '' });
+        }
       });
     }
   });
 });
+//删除地址
+router.post('/delAddress', function (req, res, next) {
+  var userId = req.cookies.userId, addressId = req.body.addressId;
+  User.update({ userId: userId }, {
+    $pull: { 'addressList': { 'addressId': addressId } }
+  }, function (err, doc) {
+    if (err) {
+      res.json({ status: '1', msg: err.message, result: '' });
+    } else {
+      res.json({ status: '0', msg: '', result: 'suc' });
+    }
+  });
+});
 
+//支付
+router.post("/payMent", function (req, res, next) {
+  var userId = req.cookies.userId,
+    addressId = req.body.addressId,
+    orderTotal = req.body.orderTotal;
+  User.findOne({ userId: userId }, function (err, doc) {
+    if (err) {
+      res.json({
+        status: "1",
+        msg: err.message,
+        result: ''
+      });
+    } else {
+      var address = '', goodsList = [];
+      //获取当前用户的地址信息
+      doc.addressList.forEach((item) => {
+        if (addressId == item.addressId) {
+          address = item;
+        }
+      })
+      //获取用户购物车的购买商品
+      doc.cartList.filter((item) => {
+        if (item.checked == '1') {
+          goodsList.push(item);
+        }
+      });
+
+      var platform = '622';
+      var r1 = Math.floor(Math.random() * 10);
+      var r2 = Math.floor(Math.random() * 10);
+
+      var sysDate = new Date().Format('yyyyMMddhhmmss');
+      var createDate = new Date().Format('yyyy-MM-dd hh:mm:ss');
+      var orderId = platform + r1 + sysDate + r2;
+      var order = {
+        orderId: orderId,
+        orderTotal: orderTotal,
+        addressInfo: address,
+        goodsList: goodsList,
+        orderStatus: '1',
+        createDate: createDate
+      };
+
+      doc.orderList.push(order);
+
+      doc.save(function (err1, doc1) {
+        if (err1) {
+          res.json({
+            status: "1",
+            msg: err.message,
+            result: ''
+          });
+        } else {
+          res.json({
+            status: "0",
+            msg: '',
+            result: {
+              orderId: order.orderId,
+              orderTotal: order.orderTotal
+            }
+          });
+        }
+      });
+    }
+  })
+});
+//根据订单Id查询订单信息
+router.get("/orderDetail", function (req, res, next) {
+  var userId = req.cookies.userId, orderId = req.param("orderId");
+  User.findOne({ userId: userId }, function (err, userInfo) {
+    if (err) {
+      res.json({
+        status: '1',
+        msg: err.message,
+        result: ''
+      });
+    } else {
+      var orderList = userInfo.orderList;
+      if (orderList.length > 0) {
+        var orderTotal = 0;
+        orderList.forEach((item) => {
+          if (item.orderId == orderId) {
+            orderTotal = item.orderTotal;
+          }
+        });
+        if (orderTotal > 0) {
+          res.json({
+            status: '0',
+            msg: '',
+            result: {
+              orderId: orderId,
+              orderTotal: orderTotal
+            }
+          })
+        } else {
+          res.json({
+            status: '120002',
+            msg: '无此订单',
+            result: ''
+          });
+        }
+      } else {
+        res.json({
+          status: '120001',
+          msg: '当前用户未创建订单',
+          result: ''
+        });
+      }
+    }
+  })
+});
 module.exports = router;
